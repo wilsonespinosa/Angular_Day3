ca9127415202243e78b6c67d748050af0a3e00d6
Wilson Espinosa Bolivar <wilson.espinosa@yuxiglobal.com>
2017-07-17 16:20:38 -0500

    service task_DOCS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+  imports: [ BrowserModule, FormsModule ],
    providers: [ HeroService ],
   bootstrap: [AppComponent]
 })
