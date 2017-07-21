88a535e95f58fee8c3db784be1c9850b0756f87a
Wilson Espinosa Bolivar <wilson.espinosa@yuxiglobal.com>
2017-07-21 15:45:01 -0500

    first commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
-import { HeroService } from './../services/hero-service.service';
-import { Hero } from './../mocks/heroes/hero';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Hero } from './../mocks/heroes/hero';
+import { HeroService } from './../services/hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,18 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
- title = 'Tour of Mythological Heroes';
   heroes: Hero[];
   selectedHero: Hero;
-  constructor(private heroService: HeroService) { }
+
+  constructor(private router: Router,
+    private heroService: HeroService) { }
+
+
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-   }
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+  }
+
+
   ngOnInit(): void {
     this.getHeroes();
   }
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 
 }
